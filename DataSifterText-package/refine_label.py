b66401620ccb07ca0fe7b31db852de3656d48e51
ymgong666 <46388276+ymgong666@users.noreply.github.com>
2021-03-26 12:12:42 -0400

    refine_label

--- DataSifterText-package/refine_label.py
+++ DataSifterText-package/refine_label.py
@@ -2,25 +2,21 @@ import csv
 import pandas as pd
 
 def refine_label(dataframe):
-	rows = []
+    rows = []
 
-	for i in range(len(dataframe)):
-		text = dataframe['text'][i]
-		label = dataframe['label'][i]
-		if label == '62':
-			label = 0
-		elif label == '42':
-			label = 1
-		elif label == '55':
-			label = 2
-		elif label == '63':
-			label = 3
-		elif label == '71':
-			label = 4
-		elif label == '1':
-			label = 0
-		elif label == '2':
-			label = 1
-		rows.append([text, label])
+    for i in range(len(dataframe)):
+        text = dataframe['text'][i]
+        label = dataframe['label'][i]
+        if label == '62':
+            label = 0
+        elif label == '42':
+            label = 1
+        elif label == '55':
+            label = 2
+        elif label == '63':
+            label = 3
+        elif label == '71':
+            label = 4
+        rows.append([text, label])
 
-	return rows
+    return rows
